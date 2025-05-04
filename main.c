2c45a30c58c98a934bfb1cae7cad35f1a159d166
giovane358 <giovanesilva.etec@gmail.com>
2025-05-03 22:04:02 -0300

    Projeto C

--- main.c
+++ main.c
@@ -1,88 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-char board[3][3]; // Tabuleiro 3x3
+#define MAX_ANIMAIS 10
 
-// Inicializa o tabuleiro com espaços em branco
-void initializeBoard() {
-    for (int i = 0; i < 3; i++)
-        for (int j = 0; j < 3; j++)
-            board[i][j] = ' ';
-}
+typedef struct {
+    char tipoAnimal[50];
+    int quantidadeAnimais;
+    float custoEstimado;
+} Animais;
 
-// Mostra o tabuleiro
-void printBoard() {
-    printf("\n");
-    for (int i = 0; i < 3; i++) {
-        printf(" %c | %c | %c ", board[i][0], board[i][1], board[i][2]);
-        if (i < 2)
-            printf("\n---|---|---\n");
-    }
-    printf("\n");
-}
+int main(void) {
+    Animais animais[MAX_ANIMAIS];
+    int n, i;
 
-// Verifica se um jogador ganhou
-char checkWinner() {
-    // Linhas e colunas
-    for (int i = 0; i < 3; i++) {
-        if (board[i][0] == board[i][1] && board[i][1] == board[i][2])
-            return board[i][0];
-        if (board[0][i] == board[1][i] && board[1][i] == board[2][i])
-            return board[0][i];
-    }
-    // Diagonais
-    if (board[0][0] == board[1][1] && board[1][1] == board[2][2])
-        return board[0][0];
-    if (board[0][2] == board[1][1] && board[1][1] == board[2][0])
-        return board[0][2];
+    printf("\nQuanto animais voce quer cadastrar?");
+    scanf("%d", &n);
 
-    return ' '; // Ninguém ganhou ainda
-}
+    for (i = 0; i < n; i++) {
 
-// Verifica se o tabuleiro está cheio
-int isFull() {
-    for (int i = 0; i < 3; i++)
-        for (int j = 0; j < 3; j++)
-            if (board[i][j] == ' ')
-                return 0;
-    return 1;
-}
+        printf("\n***Cadastro do animal %d***", i + 1);
 
-int main() {
-    int row, col;
-    char currentPlayer = 'X';
-    char winner = ' ';
+        printf("\nTipo de animais: ");
+        scanf("%s", &animais[i].tipoAnimal);
 
-    initializeBoard();
+        printf("Quantidade de %s: ", animais[i].tipoAnimal);
+        scanf("%d", &animais[i].quantidadeAnimais);
 
-    while (1) {
-        printBoard();
-        printf("Jogador %c, informe linha e coluna (0-2 0-2): ", currentPlayer);
-        scanf("%d %d", &row, &col);
-
-        // Verifica se a jogada é válida
-        if (row < 0 || row > 2 || col < 0 || col > 2 || board[row][col] != ' ') {
-            printf("Jogada inválida. Tente novamente.\n");
-            continue;
-        }
-
-        board[row][col] = currentPlayer;
-        winner = checkWinner();
-
-        if (winner != ' ') {
-            printBoard();
-            printf("Jogador %c venceu!\n", winner);
-            break;
-        }
-
-        if (isFull()) {
-            printBoard();
-            printf("Empate!\n");
-            break;
-        }
-
-        // Alterna jogador
-        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+        printf("Custo estimado de comida dos %s: ", animais[i].tipoAnimal);
+        scanf("%f", &animais[i].custoEstimado);
     }
 
+    for (i = 0; i < n; i++) {
+        float totalComidaPorAnimal = animais[i].custoEstimado / animais[i].quantidadeAnimais;
+        float totalComidaMes = animais[i].custoEstimado * 30;
+        float custo = totalComidaMes * 10;
+
+        printf("\nAnimal %s:\n", animais[i].tipoAnimal);
+        printf("- Tipo do animal: %s\n", animais[i].tipoAnimal);
+        printf("- Quantidade de animais: %d\n", animais[i].quantidadeAnimais);
+        printf("- Quantidade por kilo por animal: %.2f kg\n", totalComidaPorAnimal);
+        printf("- Quantidade por dia (total): %.2f kg\n", animais[i].custoEstimado);
+        printf("- Por mes: %.2f kg\n", totalComidaMes);
+        printf("- Custo estimado por mes: R$ %.2f\n", custo);
+    }
+
+    system("\npause");
     return 0;
+
 }
